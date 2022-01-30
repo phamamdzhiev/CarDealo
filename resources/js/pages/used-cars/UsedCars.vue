4aa59c8bfe9b08713e7b8191937679b63cb30b8c
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-01-30 12:59:13 +0200

    Part 1 done

--- resources/js/pages/used-cars/UsedCars.vue
+++ resources/js/pages/used-cars/UsedCars.vue
@@ -1,96 +1,99 @@
 <template>
     <div>
-        <base-carousel :height="400"
-                       :bannerImage="'https://stimg.cardekho.com/pwa/img/bgimg/used-car-hero-img.jpg'"></base-carousel>
+        <base-carousel
+            :height="400"
+            :bannerImage="'https://stimg.cardekho.com/pwa/img/bgimg/used-car-hero-img.jpg'"
+        ></base-carousel>
+
         <BaseCard>
             <h5 class="fw-bold">Употребявани автомобили</h5>
             <p>
                 Намерете пълен списък на сертифицирани употребявани автомобили в
-                България. Можете да изберете коли втора употреба, като приложите
-                филтри като местоположение, цена, тип каросерия, марка и т.н.
+                България. Можете да изберете коли втора употреба, като приложите филтри
+                като местоположение, цена, тип каросерия, марка и т.н.
             </p>
         </BaseCard>
 
         <BaseCard>
             <h5 class="fw-bold">Препоръчани автомобили за теб</h5>
-            <Carousel :settings="sliderConfig">
-                <Slide v-for="slide in 10" :key="slide">
-                    <div class="carousel__item">
-                        <CarSingleItem></CarSingleItem>
-                    </div>
-                </Slide>
-                <template #addons>
-                    <Navigation/>
-                </template>
-            </Carousel>
+            <hr/>
+            <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
         </BaseCard>
 
         <BaseCard>
             <h5 class="fw-bold">Автомобили в България по град</h5>
-
+            <hr/>
+            <PopularCities></PopularCities>
         </BaseCard>
+
 
         <BaseCard>
             <h5 class="fw-bold">Автомобили според бюджет</h5>
-
+            <hr/>
+            <Tabs :options="{ useUrlFragment: false }">
+                <Tab name="до 5000 BGN">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+                <Tab name="5000 - 10 000 BGN">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+                <Tab name="над 10 000 BGN">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+            </Tabs>
         </BaseCard>
 
         <BaseCard>
             <h5 class="fw-bold">Автомобили по каросерия</h5>
-
+            <hr>
+            <Tabs :options="{ useUrlFragment: false }">
+                <Tab name="Хечбек">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+                <Tab name="Седан">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+                <Tab name="Комби">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+                <Tab name="SUV">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+                <Tab name="Купе">
+                    <UsedCarsVue3Carousel></UsedCarsVue3Carousel>
+                </Tab>
+            </Tabs>
         </BaseCard>
-
         <BaseCard>
             <!--            interna add-->
-
         </BaseCard>
 
         <BaseCard>
             <h5 class="fw-bold">Автомобили по вид на двигателя</h5>
-
         </BaseCard>
     </div>
 </template>
 <script>
-import {Carousel, Slide, Navigation} from 'vue3-carousel';
-import 'vue3-carousel/dist/carousel.css';
 import BaseCarousel from "../../components/ui/base/BaseCarousel.vue";
 import BaseCard from "../../components/ui/base/BaseCard.vue";
-import CarSingleItem from "../../components/car/CarSingleItem";
-import {onMounted, reactive, ref} from "vue";
+
+import UsedCarsVue3Carousel from "./sliders/UsedCarsVue3Carousel";
+import PopularCities from "./partials/PopularCities.vue";
+import {reactive, ref} from "vue";
+import {Tab, Tabs} from 'vue3-tabs-component';
+
 
 export default {
     components: {
         BaseCarousel,
         BaseCard,
-        CarSingleItem,
-        Carousel,
-        Slide,
-        Navigation
+        PopularCities,
+        UsedCarsVue3Carousel,
+        Tab,
+        Tabs
     },
     setup() {
-        const sliderConfig = {
-            itemsToShow: 4,
-            snapAlign: 'start',
-        }
-
-        const data = reactive({
-            name: 0
-        });
-
-        const a = ref([
-            {name: 'Petar', age: 24},
-            {name: 'Raddy', age: 21},
-        ])
-
-        onMounted(() => {
-            console.log('mounted');
-        });
-
-        return {
-            data,
-            sliderConfig
-        }
     }
 };
 </script>
+
---
+++ resources/js/pages/used-cars/sliders/UsedCarsVue3Carousel.vue
@@ -0,0 +1,72 @@
+<template>
+    <Swiper :breakpoints="swiperBreakPoints"
+            :slides-per-view="4"
+            :space-between="40"
+            :navigation="true"
+            :modules="modules"
+            virtual>
+        <SwiperSlide v-for="slide in 9" :key="slide" :virtualIndex="slide">
+            <CarSingleItem></CarSingleItem>
+        </SwiperSlide>
+    </Swiper>
+</template>
+
+<script>
+import CarSingleItem from "../../../components/car/CarSingleItem";
+import {Swiper, SwiperSlide} from "swiper/vue";
+import {Navigation, Virtual} from "swiper";
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+export default {
+    name: "UsedCarsCarousels",
+    components: {
+        Swiper, SwiperSlide, CarSingleItem
+    },
+
+
+    setup() {
+        const swiperModules = [Navigation, Virtual];
+        const swiperBreakPoints = {
+            0: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1024: {
+                slidesPerView: 4,
+            }
+        }
+        return {
+            modules: swiperModules,
+            swiperBreakPoints
+        }
+
+    }
+}
+</script>
+
+<style>
+.carousel__track {
+    padding: 7px 0;
+}
+
+.carousel__slide {
+    /*padding: 0 5px;*/
+}
+
+.carousel__prev--in-active,
+.carousel__next--in-active {
+    display: none;
+}
+
+.carousel__prev,
+.carousel__next {
+    background-color: #fd5750;
+    border: 4px solid white;
+}
+</style>
