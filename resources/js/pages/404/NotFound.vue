9bb96fa859871a9459e221dbc09f3ec3d778c81f
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-02-06 12:33:05 +0200

    No found page

--- resources/js/pages/404/NotFound.vue
+++ resources/js/pages/404/NotFound.vue
@@ -1,13 +1,48 @@
 <template>
-<h1>not found pag 404</h1>
+    <div id="404">
+        <div class="container">
+            <div id="background"
+                 class="d-flex justify-content-center align-items-center flex-column"
+                 :style="{ backgroundImage: 'url(' + asset('storage/404_page.png') + ')'}">
+                <div class="text-end">
+                    <h1 class="text-base-color fw-bold">Ооопс...</h1>
+                    <p class="text-secondary-color fs-4 mb-5">
+                        Изглежда не можем да намерим страницата, която търсите.
+                    </p>
+                    <router-link class="base-button" :to="{name: 'Home'}">
+                        Начална страница
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import assetMixin from '../../mixins/asset';
+import BaseButton from "../../components/ui/base/BaseButton";
 export default {
-    name: "NotFound"
+    name: "NotFound",
+    mixins: [assetMixin],
+    components: {
+        BaseButton
+    }
 }
 </script>
 
 <style scoped>
+#background {
+    padding: 3rem 1rem;
+    min-height: 400px;
+    background-size: contain;
+    background-repeat: no-repeat;
+}
 
+h1 {
+    font-size: 4.2rem;
+}
+
+.text-end {
+    max-width: 500px;
+}
 </style>
