fa141bbeea23f11c61d30c1ea5b49652f346cfff
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-03-19 11:48:27 +0200

    Finish with FAQ section

--- resources/js/pages/faq/FAQ.vue
+++ resources/js/pages/faq/FAQ.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="custom-container-sm">
-        <page-heading heading="Често задавани въпроси"></page-heading>
+        <page-heading heading="Често задавани въпроси (FAQ)"></page-heading>
         <base-card>
-            <p>Comming soon...</p>
+            <FAQItem v-for="item in QA" :answer="item.a" :question="item.q"></FAQItem>
         </base-card>
     </div>
 </template>
@@ -10,12 +10,25 @@
 <script>
 import PageHeading from "../../components/layout/PageHeading";
 import BaseCard from "../../components/ui/base/BaseCard";
+import {ref} from "vue";
+import FAQItem from "./FAQItem";
+
 export default {
     name: "FAQ",
     components: {
         PageHeading,
         BaseCard,
+        FAQItem
+    },
+    setup() {
+        const QA = ref([
+            {id: 1, q: 'Kak se kazvash?', a: 'Starhil'},
+            {id: 2, q: 'Kak se kazvash 1', a: 'Starhil 1'},
+        ]);
 
+        return {
+            QA
+        }
     }
 }
 </script>
---
+++ resources/js/pages/faq/FAQItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h6 class="fw-bold m-0">Въпрос <i class="fa-solid fa-question ps-1"></i></h6>
+        <p class="faq-question text-base-color mb-2">{{ question }}</p>
+        <h6 class="fw-bold m-0">Отговор <i class="fa-solid fa-comment-dots ps-1"></i></h6>
+        <p class="faq-answer text-base-color mb-2">{{ answer }}</p>
+        <hr/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FAQItem",
+    props: {
+        question: {
+            type: String,
+            required: true,
+        },
+        answer: {
+            type: String,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+h6.m-0 {
+    font-size: 14px;
+}
+</style>
