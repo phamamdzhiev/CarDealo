6bab1c50e1adc37eceb55c06eb67bf98f08e785c
IvanBozhakov <ivanbozhakov@gmail.com>
2022-04-03 17:38:39 +0300

    Show car model/brand name

--- app/Models/Repository/OfferRepository.php
+++ app/Models/Repository/OfferRepository.php
@@ -20,19 +20,19 @@ class OfferRepository
             'vehicles.hp',
             'vehicles.km',
             'vehicles.cm3',
-            'cities.name',
-            'images.image'
+            'cities.name as city',
+            'images.image',
+            'brands.name as brand',
+            'models.name as model'
         ]
     ): Builder
     {
         return DB::table('offers')
                 ->join('vehicles', 'vehicles.offer_id', '=', 'offers.id')
                 ->join('images', 'images.offer_id', '=', 'offers.id')
-            /**
-             * Vanko tyk slojih where, shototo inache hvash vsichki snimki i vav FO mi vrushta edna obqva po nqkolko puti cikala,
-             * ne znam dali tyk trqbvashe da se sloji no ROBOTI, za sq. Ako nes go opravi pls.
-             */
-                        ->where('images.is_main', '=', 1)
+                ->join('brands', 'vehicles.brand_id', '=', 'brands.id')
+                ->join('models', 'vehicles.model_id', '=', 'models.id')
+                ->where('images.is_main', '=', 1)
                 ->join('cities', 'cities.id', '=', 'offers.city_id')
                 ->join('vehicles_types', 'vehicles.type_id', '=', 'vehicles_types.id')
                 ->join('merchants', 'merchants.user_id', '=', 'offers.user_id')
